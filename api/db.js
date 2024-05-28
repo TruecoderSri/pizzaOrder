864dd8ced3efb6b204d8cd9d2f4c749542a15e18
Srijan <srijan2016403221@ipu.ac.in>
2024-05-28 17:42:24 +0530

    env changes

--- api/db.js
+++ api/db.js
@@ -1,10 +1,5 @@
 const mongoose = require("mongoose");
-<<<<<<< HEAD
-var mongoURL = process.env.MONGO_URL;
-=======
-const mongoURL = process.env.mongoURL;
-
->>>>>>> 031162901cac43b9bf577a203b1202eb8e1dd8f8
+const mongoURL = process.env.MONGO_URL;
 mongoose.connect(mongoURL, { useUnifiedTopology: true, useNewUrlParser: true });
 
 var db = mongoose.connection;
